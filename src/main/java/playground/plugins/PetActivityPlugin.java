bc6f977f1d6b4e39f789cf636089235cec4b045d
itaii <itaii@DESKTOP-1QCMHO4.mynet>
2018-12-21 08:44:07 +0200

    add PetAcitivtyPlugin class

---
+++ src/main/java/playground/plugins/PetActivityPlugin.java
@@ -0,0 +1,45 @@
+package playground.plugins;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import playground.jpadal.ElementDao;
+import playground.jpadal.UserDao;
+import playground.logic.Entities.ActivityEntity;
+import playground.logic.Entities.ElementEntity;
+import playground.logic.Entities.UserEntity;
+import playground.logic.jpa.ElementNotFoundException;
+import playground.logic.jpa.UserNotFoundException;
+
+@Component
+public class PetActivityPlugin implements PlaygroungActivityPlugin {
+	private UserDao users;
+	private ElementDao elements;
+
+	@Autowired
+	public PetActivityPlugin(UserDao users, ElementDao elements) {
+		this.users = users;
+		this.elements = elements;
+	}
+
+	@Override
+	public Object invokeAction(ActivityEntity activity) {
+		String UserKey = activity.getPlayerPlayground() + "@@" + activity.getPlayerEmail();
+		UserEntity user = this.users.findById(UserKey)
+				.orElseThrow(() -> new UserNotFoundException("no user found for: " + UserKey));
+
+		// set key for element
+		String element_key = activity.getElementPlayground() + "@@" + activity.getElementId();
+		ElementEntity element = this.elements.findById(element_key)
+				.orElseThrow(() -> new ElementNotFoundException("no Element for: " + element_key));
+
+		if ("Animal".equalsIgnoreCase(element.getType())) {
+			// Add point to user
+		} else {
+			throw new RuntimeException("Not an Animal!");
+		}
+
+		return activity;
+	}
+
+}
