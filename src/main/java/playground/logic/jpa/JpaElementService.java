319bcebfcfb5c1be4003ad92f400a3f17bd874a8
itaii <itaii@DESKTOP-1QCMHO4.mynet>
2018-12-21 08:36:44 +0200

    Add expirationDate support for manager and ignore at GetElement

--- src/main/java/playground/logic/jpa/JpaElementService.java
+++ src/main/java/playground/logic/jpa/JpaElementService.java
@@ -19,16 +19,15 @@ import playground.logic.Entities.UserEntity;
 import playground.logic.Services.PlaygroundElementService;
 import java.util.Date;
 
-
 @Service
 public class JpaElementService implements PlaygroundElementService {
-			
+
 	private NumbersDao numbers;
 	private ElementDao elements;
 	private UserDao users;
 
 	@Autowired
-	public JpaElementService(NumbersDao numbers,ElementDao elements,UserDao users) {
+	public JpaElementService(NumbersDao numbers, ElementDao elements, UserDao users) {
 		super();
 		this.elements = elements;
 		this.users = users;
@@ -38,148 +37,128 @@ public class JpaElementService implements PlaygroundElementService {
 	@Override
 	@Transactional
 	@ManagerExistCheck
-	public ElementEntity addNewElement(String userPlayground, String email,ElementEntity elementEntity) throws ElementAlreadyExistException {
-		
-		if(!elements.existsById(elementEntity.getKey()))
-		{
+	public ElementEntity addNewElement(String userPlayground, String email, ElementEntity elementEntity)
+			throws ElementAlreadyExistException {
+
+		if (!elements.existsById(elementEntity.getKey())) {
 			long number = this.numbers.save(new GeneratedNumber()).getNextValue();
-			this.numbers.deleteById(number);			
-			elementEntity.setId(number+"");		
-			
+			this.numbers.deleteById(number);
+			elementEntity.setId(number + "");
+
 			return this.elements.save(elementEntity);
 
-		}else {
+		} else {
 			throw new ElementAlreadyExistException("elementEntity exisits with: " + elementEntity.getKey());
 		}
-		
+
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	@UserVerifiedAndExistCheck
-	public ElementEntity getElement(String userPlayground, String email,String element_id, String element_Playground) throws ElementNotFoundException {
-		// set key for element 
-		String element_key = element_Playground + "@@" +element_id;
-		
-		return 
-				this.elements.findById(element_key)
-				.orElseThrow(()->new ElementNotFoundException("no Element for: " + element_key));
+	public ElementEntity getElement(String userPlayground, String email, String element_id, String element_Playground)
+			throws ElementNotFoundException {
+		// set key for element
+		String element_key = element_Playground + "@@" + element_id;
+		if ("Manager".equalsIgnoreCase(GetUserRole(userPlayground, email))) {
+			return this.elements.findById(element_key)
+					.orElseThrow(() -> new ElementNotFoundException("no Element for: " + element_key));
+		} else {
+			// if it's a Client
+			Date today = new Date();
+			return this.elements.findAllByExirationDateIsNullOrExirationDateAfter(today,
+					PageRequest.of(0, 1, Direction.DESC, "creationDate")).getContent().get(0);
+		}
 
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	@UserVerifiedAndExistCheck
 	@MyLogger
-	public List<ElementEntity> getAllElements(String userPlayground,String email,int size, int page) {
-		
-		
-		if("Manager".equalsIgnoreCase(GetUserRole(userPlayground,email)))
-		{
-			//if it's a Manager
-			return 
-			this.elements.findAll(PageRequest.of(page, size, Direction.DESC, "creationDate"))
-				.getContent();
-		}
-		else
-		{
-			//if it's a Client
+	public List<ElementEntity> getAllElements(String userPlayground, String email, int size, int page) {
+
+		if ("Manager".equalsIgnoreCase(GetUserRole(userPlayground, email))) {
+			// if it's a Manager
+			return this.elements.findAll(PageRequest.of(page, size, Direction.DESC, "creationDate")).getContent();
+		} else {
+			// if it's a Client
 			Date today = new Date();
-			return this.elements.findAllByExirationDateIsNullOrExirationDateAfter(
-					today
-					,PageRequest.of(
-					page, 
-					size, 
-					Direction.DESC, 
-					"creationDate"))
-			.getContent();
+			return this.elements.findAllByExirationDateIsNullOrExirationDateAfter(today,
+					PageRequest.of(page, size, Direction.DESC, "creationDate")).getContent();
 		}
-		
-
-
 
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	@UserVerifiedAndExistCheck
 	@MyLogger
-	public List<ElementEntity> getAllNearElements(String userPlayground,String email,double x, double y, double distance, int size, int page) {
-		
-		Double upperX = (x+distance);
-		Double lowerX = (x-distance);
-		Double upperY = (y+distance);
-		Double lowerY = (y-distance);
+	public List<ElementEntity> getAllNearElements(String userPlayground, String email, double x, double y,
+			double distance, int size, int page) {
 
-		
-		if("Manager".equalsIgnoreCase(GetUserRole(userPlayground,email)))
-		{
-			//if it's a Manager
-			return this.elements.findAllByXLessThanAndXGreaterThanAndYLessThanAndYGreaterThan(upperX,lowerX,upperY,lowerY
-					,PageRequest.of(
-					page, 
-					size, 
-					Direction.DESC, 
-					"creationDate"))
-			.getContent();	
-		}
-		else
-		{
-			//if it's a Client
+		Double upperX = (x + distance);
+		Double lowerX = (x - distance);
+		Double upperY = (y + distance);
+		Double lowerY = (y - distance);
+
+		if ("Manager".equalsIgnoreCase(GetUserRole(userPlayground, email))) {
+			// if it's a Manager
+			return this.elements.findAllByXLessThanAndXGreaterThanAndYLessThanAndYGreaterThan(upperX, lowerX, upperY,
+					lowerY, PageRequest.of(page, size, Direction.DESC, "creationDate")).getContent();
+		} else {
+			// if it's a Client
 			Date today = new Date();
-			return this.elements.findAllByExirationDateIsNullAndXLessThanAndXGreaterThanAndYLessThanAndYGreaterThanOrExirationDateAfterAndXLessThanAndXGreaterThanAndYLessThanAndYGreaterThan(upperX,lowerX,upperY,lowerY
-					,today,upperX,lowerX,upperY,lowerY
-					,PageRequest.of(
-					page, 
-					size, 
-					Direction.DESC, 
-					"creationDate"))
-			.getContent();
+			return this.elements
+					.findAllByExirationDateIsNullAndXLessThanAndXGreaterThanAndYLessThanAndYGreaterThanOrExirationDateAfterAndXLessThanAndXGreaterThanAndYLessThanAndYGreaterThan(
+							upperX, lowerX, upperY, lowerY, today, upperX, lowerX, upperY, lowerY,
+							PageRequest.of(page, size, Direction.DESC, "creationDate"))
+					.getContent();
 		}
-				
+
 	}
 
 	@Override
 	@Transactional
 	@ManagerExistCheck
 	@MyLogger
-	public void updateElement(String userPlayground,String email,ElementEntity updatedElementEntity, String playground, String id) throws Exception {
-		
-		ElementEntity existing = getElement( userPlayground,  email,id, playground);
-			
-		
+	public void updateElement(String userPlayground, String email, ElementEntity updatedElementEntity,
+			String playground, String id) throws Exception {
+
+		ElementEntity existing = getElement(userPlayground, email, id, playground);
+
 		if (updatedElementEntity.getX() != null) {
 			existing.setX(updatedElementEntity.getX());
 		}
-		
+
 		if (updatedElementEntity.getY() != null) {
 			existing.setY(updatedElementEntity.getY());
-		}	
-		
+		}
+
 		if (updatedElementEntity.getName() != null) {
 			existing.setName(updatedElementEntity.getName());
 		}
-		
+
 		if (updatedElementEntity.getExirationDate() != null) {
 			existing.setExirationDate(updatedElementEntity.getExirationDate());
 		}
-		
+
 		if (updatedElementEntity.getType() != null) {
 			existing.setType(updatedElementEntity.getType());
 		}
-		
+
 		if (updatedElementEntity.getAttributes() != null) {
 			existing.setAttributes(updatedElementEntity.getAttributes());
 		}
-		
+
 		this.elements.save(existing);
-		
+
 	}
 
 	@Override
 	@MyLogger
 	public void validateElementAttribteName(String name) throws ElementAttributeNotValidException {
-		if(!name.equals("name") && !name.equals("type"))
+		if (!name.equals("name") && !name.equals("type"))
 			throw new ElementAttributeNotValidException("Invalid Attribute for searching elements");
 	}
 
@@ -187,74 +166,47 @@ public class JpaElementService implements PlaygroundElementService {
 	@Transactional
 	@MyLogger
 	public void cleanup() {
-		
+
 		this.elements.deleteAll();
 	}
 
 	@Override
 	@UserVerifiedAndExistCheck
 	@MyLogger
-	public List<ElementEntity> getElementsWithAttribute(String userPlayground,String email, String attributeName, String value, int size, int page) {
+	public List<ElementEntity> getElementsWithAttribute(String userPlayground, String email, String attributeName,
+			String value, int size, int page) {
 		// if attribute is "name"
-		//if(isManager) {
-			if(attributeName.equals("name")) {
-				return this.elements.findAllByNameLike(
-						value,
-						PageRequest.of(
-						page, 
-						size, 
-						Direction.DESC, 
-						"creationDate"))
-				.getContent();	
-			}else {
-				// attribute is "type"
-				return this.elements.findAllByTypeLike(
-						value,
-						PageRequest.of(
-						page, 
-						size, 
-						Direction.DESC, 
-						"creationDate"))
-				.getContent();	
-			}
-		//}else { //Player
-/*			Date today = new Date();
-			if(attributeName.equals("name")) {
-				return this.elements.findAllByExirationDateIsNullAndNameLikeOrExirationDateAfterAndNameLike(
-						today,
-						value,
-						PageRequest.of(
-						page, 
-						size, 
-						Direction.DESC, 
-						"creationDate"))
-				.getContent();	
-			}else {
-				// attribute is "type"
-				return this.elements.findAllByExirationDateIsNullAndTypeLikeOrExirationDateAfterAndTypeLike(
-						today,
-						value,
-						PageRequest.of(
-						page, 
-						size, 
-						Direction.DESC, 
-						"creationDate"))
-				.getContent();	
-			}*/
-	//	}
-		
+		// if(isManager) {
+		if (attributeName.equals("name")) {
+			return this.elements.findAllByNameLike(value, PageRequest.of(page, size, Direction.DESC, "creationDate"))
+					.getContent();
+		} else {
+			// attribute is "type"
+			return this.elements.findAllByTypeLike(value, PageRequest.of(page, size, Direction.DESC, "creationDate"))
+					.getContent();
+		}
+		// }else { //Player
+		/*
+		 * Date today = new Date(); if(attributeName.equals("name")) { return
+		 * this.elements.
+		 * findAllByExirationDateIsNullAndNameLikeOrExirationDateAfterAndNameLike(
+		 * today, value, PageRequest.of( page, size, Direction.DESC, "creationDate"))
+		 * .getContent(); }else { // attribute is "type" return this.elements.
+		 * findAllByExirationDateIsNullAndTypeLikeOrExirationDateAfterAndTypeLike(
+		 * today, value, PageRequest.of( page, size, Direction.DESC, "creationDate"))
+		 * .getContent(); }
+		 */
+		// }
+
 	}
 
 	@Override
 	public String GetUserRole(String userPlayground, String email) throws UserNotFoundException {
-		String key = userPlayground+"@@"+email;
+		String key = userPlayground + "@@" + email;
 		UserEntity User = this.users.findById(key)
-		.orElseThrow(()->new UserNotFoundException("no user found for: " + key));
-		
+				.orElseThrow(() -> new UserNotFoundException("no user found for: " + key));
+
 		return User.getRole();
 	}
-	
-	
-	
 
 }
