334d3908879631172173b96b83bbecef682d951d
Sapir <Sapir@192.168.1.8>
2019-01-06 13:07:08 +0200

    Changed email service to component

--- src/main/java/playground/logic/Services/EmailService.java
+++ src/main/java/playground/logic/Services/EmailService.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import playground.logic.Entities.UserEntity;
 
-@Service
+@Component
 public class EmailService {
   
     public JavaMailSender javaMailSender;
